0cb92f633a36505e467b7476496e94b10bc5bd26
John Gill <jgill@arista.com>
2025-07-01 21:11:55 +0000

    populate vlans1

--- python/add_vlans.py
+++ python/add_vlans.py
@@ -7,7 +7,7 @@ file = open('vlans.yml', 'r')
 pyeapi.load_config('eapi.conf')
 vlan_dict = yaml.safe_load(file)
 
-for switch in vlan_dict['switches']:
+for switch in vlan_dict['devices']:
     print(f"Connecting to {switch}")
     connect = pyeapi.connect_to(switch)
     vlan_api = connect.api('vlans')
